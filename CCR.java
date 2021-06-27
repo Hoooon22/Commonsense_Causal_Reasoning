5ee6a0fca7f7981308b9e6f73bae0c58628488bc
Hoooon22 <momo990305@gmail.com>
2021-06-27 23:53:11 +0900

    method getFreq

--- CCR.java
+++ CCR.java
@@ -1,11 +1,37 @@
 import java.util.*;
 import java.io.*;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class CCR {
+    public int getFreq(ArrayList<String> a, ArrayList<String> b, ArrayList<Integer> freq, String strA, String strB)
+    {
+        int num = -1; // If no value, return -1
+        int i = 0;
+        if (a.contains(strA) && b.contains(strB)) 
+        {
+            while(i < a.size())
+            {
+                if (a.get(i) == strA && b.get(i) == strB)
+                {
+                    num = i;
+                    break;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        } 
+        else 
+        {
+            num = -1;
+        }
+
+        return num;
+    }
+
     public static void main(String[] args) 
     {
         ArrayList<String> a = new ArrayList<String>();
@@ -40,6 +66,10 @@ public class CCR {
         }catch ( IOException e ) {
             System.out.println(e);
         } 
+
+        // freq index
+        CCR myCCR = new CCR();
+        System.out.println(myCCR.getFreq(a, b, freq, "cleveland", "debt"));
     }
 }
 
